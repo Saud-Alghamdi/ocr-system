2c6444dcf9b4f92a75c71967a9d3fac7bbd54604
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2024-01-07 22:22:42 +0300

    create the routes for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import Layout from "./layouts/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import OcrPage from "./pages/OcrPage";
+import ResultPage from "./pages/ResultPage";
 
 export default function App() {
   return (
-    <Layout/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/ocr"
+          element={<OcrPage />}
+        />
+        <Route
+          path="/result"
+          element={<ResultPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
