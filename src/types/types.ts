9034355a79d9fc81773109401a65d77e40ccaf64
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2024-01-07 22:26:55 +0300

    Create types

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type ButtonProps = {
+  text: string;
+  additionalTwStyles?: string;
+  onClick?: () => void;
+};
+
+export type LoaderProps = {
+  text: string;
+};
+
+export type LocationState = {
+  extractedText: string;
+};
