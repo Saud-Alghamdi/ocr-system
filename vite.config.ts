d4627fd0f002c9afb033dda1ecb4f5a5e9ffe498
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2024-01-07 22:54:19 +0300

    update base to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/saud-alghamdi/",
+  base: "/ocr-system/",
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Listen on all network interfaces
