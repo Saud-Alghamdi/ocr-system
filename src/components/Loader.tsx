037cd91a9d20699e2c8f3d202a46ee14fd422430
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2024-01-08 11:11:29 +0300

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,11 @@
 import { LoaderProps } from "../types/types";
 
 export default function Loader({ text }: LoaderProps) {
-  const loaderClasses = "w-16 h-16 mb-4 ease-linear border-8 border-t-8 border-gray-200 rounded-full";
+  const loaderClasses = "w-16 h-16 mb-4 ease-linear rounded-full loader"; // Added 'loader' class here
 
   return (
     <div className="flex items-center justify-center mt-4">
-      <div className={loaderClasses}></div>
+      <div className={`${loaderClasses} border-8 border-t-8 border-gray-200 border-t-blue-600`}></div> {/* Adjusted border colors */}
       <div className="mx-2 text-center text-gray-700">{text}</div>
     </div>
   );
